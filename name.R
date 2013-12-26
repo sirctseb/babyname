7e0652078731afc02b72191abf7dc3ef4fb3a67b
Christopher Best <bestchris@gmail.com>
2013-12-26 14:20:10 -0500

    calculate bigrams

--- name.R
+++ name.R
@@ -9,11 +9,11 @@ learn <- function() {
 	namelist$name <- tolower(as.character(namelist$name))
 	namelist.f <- subset(namelist, gender == 'F')
 	namelist.m <- subset(namelist, gender == 'M')
-	res$unigram.f <- aggregate(namelist.f)
-	res$unigram.m <- aggregate(namelist.m)
-	res$unigram <- list()
-	for(l in names(res$unigram.f)) {
-		res$unigram[[l]] <- res$unigram.m[[l]] + res$unigram.f[[l]]
+	res$ngram.f <- aggregate(namelist.f)
+	res$ngram.m <- aggregate(namelist.m)
+	res$ngram <- list()
+	for(l in letters) {
+		res$ngram$gram[[l]] <- res$ngram.m$gram[[l]] + res$ngram.f$gram[[l]]
 	}
 	res
 }
@@ -21,22 +21,29 @@ aggregate <- function(namelist) {
 	# initialize unigram list
 	letterlist <- sample(0,26,rep=TRUE)
 	names(letterlist) <- letters
-	unigram <- list()
-	unigram[['_']] <- letterlist
+	ngram <- list()
+	ngram$gram <- letterlist
 	for(l in letters) {
-		unigram[[l]] <- letterlist
+		ngram[[l]] <- list()
+		ngram[[l]]$gram <- letterlist
+		for(d in letters) {
+			ngram[[l]][[d]]$gram <- letterlist
+		}
 	}
 	# get statistics
 	for(i in seq(namelist$name)) {
 		split = strsplit(namelist[i,1], '')[[1]]
 		# increment frequencyf of first letter
-		unigram[['_']][[split[1]]] <- unigram[['_']][[split[1]]] + 1
+		ngram$gram[[split[1]]] <- ngram$gram[[split[1]]] + 1
 		for(j in 2:length(split)) {
 			# increment frequency of letter based on previous unigram
-			unigram[[split[j - 1]]][[split[j]]] <- unigram[[split[j-1]]][[split[j]]] + 1
+			ngram[[split[j - 1]]]$gram[[split[j]]] <- ngram[[split[j-1]]]$gram[[split[j]]] + 1
+			if(j > 2) {
+				ngram[[split[j - 2]]][[split[j - 1]]]$gram[[split[j]]] <- ngram[[split[j - 2]]][[split[j - 1]]]$gram[[split[j]]] + 1
+			}
 		}
 	}
-	unigram
+	ngram
 }
 
 name <- function(stats, len = FALSE, count = 1, gender = 'both') {
@@ -54,17 +61,17 @@ name <- function(stats, len = FALSE, count = 1, gender = 'both') {
 	}
 	names
 }
-onename <- function(unigram, len = FALSE) {
+onename <- function(ngram, len = FALSE) {
 	if(len == FALSE) {
 		# todo magic numbers to define range of name lengths
 		len <- sample(4:8, 1)
 	}
 	# generate first letter based on frequency
-	name <- sample(letters, 1, prob = stats$unigram[['_']])
+	name <- sample(letters, 1, prob = stats$ngram$gram)
 	len <- len - 1
 	# append letters until len
 	while(len > 0) {
-		name <- append(name, sample(letters, 1, prob = stats$unigram[[tail(name, 1)]]))
+		name <- append(name, sample(letters, 1, prob = stats$ngram[[tail(name, 1)]]$gram))
 		len <- len - 1
 	}
 	paste(name, collapse = '')

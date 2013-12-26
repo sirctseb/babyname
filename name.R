f641de6c50b028cac99f27c9136602693de660d2
Christopher Best <bestchris@gmail.com>
2013-12-26 13:21:27 -0500

    optionally separate by gender

--- name.R
+++ name.R
@@ -6,36 +6,56 @@ learn <- function() {
 	# create unigram structure
 	letterlist <- sample(0,26,rep=TRUE)
 	names(letterlist) <- letters
-	res$unigram <- list()
-	res$unigram[['_']] <- letterlist
-	for(l in letters) {
-		res$unigram[[l]] <- letterlist
-	}
 	# get name list
 	namelist <- read.table('names/yob2012.txt', sep=',', header=FALSE)
 	names(namelist) <- c('name', 'gender', 'count')
 	namelist$name <- tolower(as.character(namelist$name))
+	namelist.f <- subset(namelist, gender == 'F')
+	namelist.m <- subset(namelist, gender == 'M')
+	res$unigram.f <- aggregate(namelist.f)
+	res$unigram.m <- aggregate(namelist.m)
+	res$unigram <- list()
+	for(l in names(res$unigram.f)) {
+		res$unigram[[l]] <- res$unigram.m[[l]] + res$unigram.f[[l]]
+	}
+	res
+}
+aggregate <- function(namelist) {
+	# initialize unigram list
+	unigram <- list()
+	unigram[['_']] <- letterlist
+	for(l in letters) {
+		unigram[[l]] <- letterlist
+	}
 	# get statistics
 	for(i in seq(namelist$name)) {
 		split = strsplit(namelist[i,1], '')[[1]]
 		# increment frequencyf of first letter
-		res$unigram[['_']][[split[1]]] <- res$unigram[['_']][[split[1]]] + 1
+		unigram[['_']][[split[1]]] <- unigram[['_']][[split[1]]] + 1
 		for(j in 2:length(split)) {
 			# increment frequency of letter based on previous unigram
-			res$unigram[[split[j - 1]]][[split[j]]] <- res$unigram[[split[j-1]]][[split[j]]] + 1
+			unigram[[split[j - 1]]][[split[j]]] <- unigram[[split[j-1]]][[split[j]]] + 1
 		}
 	}
-	res
+	unigram
 }
 
-name <- function(stats, len = FALSE, count = 1) {
+name <- function(stats, len = FALSE, count = 1, gender = 'both') {
+	if(gender == 'both') {
+		# TODO combine data
+		un <- stats$unigram
+	} else if(head(gender,1) == 'm') {
+		un <- stats$unigram.m
+	} else {
+		un <- stats$unigram.f
+	}
 	names <- c()
 	for(i in seq(count)) {
-		names <- append(names, onename(stats, len))
+		names <- append(names, onename(un, len))
 	}
 	names
 }
-onename <- function(stats, len = FALSE) {
+onename <- function(unigram, len = FALSE) {
 	if(len == FALSE) {
 		# todo magic numbers to define range of name lengths
 		len <- sample(4:8, 1)
